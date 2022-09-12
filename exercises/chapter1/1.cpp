47268f264f28e75b29966ff0657a0cc6fa059bc5
andif <fzucwj@gmail.com>
2022-09-12 09:50:37 +0800

    c1:1 done

--- exercises/chapter1/1.cpp
+++ exercises/chapter1/1.cpp
@@ -2,29 +2,15 @@
  *    author: andif
  *    created: 11.09.2022 14:06:48
 **/
-#include<bits/stdc++.h>
-using namespace std;
 
-#define de(x) cerr << #x << " = " << x << endl
-#define dd(x) cerr << #x << " = " << x << " "
-#define rep(i, a, b) for(int i = a; i < b; ++i)
-#define per(i, a, b) for(int i = a; i > b; --i)
-#define mt(a, b) memset(a, b, sizeof(a))
-#define sz(a) (int)a.size()
-#define fi first
-#define se second
-#define qc ios_base::sync_with_stdio(0);cin.tie(0)
-#define eb emplace_back
-#define all(a) a.begin(), a.end()
-using ll = long long;
-using db = double;
-using pii = pair<int, int>;
-using pdd = pair<db, db>;
-using pll = pair<ll, ll>;
-using vi = vector<int>;
-const db eps = 1e-9;
+// while
+e1;
+while (e2) {
+	e3;
+}
 
-int main() {
-
-    return 0;
+// do while
+e1
+if (e2) {
+	do {e3} while(e2);
 }
