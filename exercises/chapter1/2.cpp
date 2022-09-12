5f4c74fb38fff6bf8773d2ae3a946423f16e635f
andif <fzucwj@gmail.com>
2022-09-12 09:53:10 +0800

    c1:2 done

---
+++ exercises/chapter1/2.cpp
@@ -0,0 +1,12 @@
+/**
+ *    author: andif
+ *    created: 12.09.2022 09:51:52
+**/
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	for (unsigned int i = 0; i < std::numeric_limits<unsigned int>::max(); ++i) {
+	}
+    return 0;
+}
